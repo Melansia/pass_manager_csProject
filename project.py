7fc3e1462d49b29eee8ed4baa86821f0a66d4182
Greg L <lupasco.grigori@gmail.com>
2023-06-25 15:56:05 +0300

    file renamed, cleaned from not features that was not yet added, + fun to create a database

--- project.py
+++ project.py
@@ -1,4 +1,21 @@
+import sqlite3
+
 from registration import user_registration, login_user, pm
+from dir_check_create import create_directory
+
+
+def create_db(user_path="./Users/user_login.db"):
+    conn = sqlite3.connect(user_path)
+    cur = conn.cursor()
+
+    cur.execute("""
+    CREATE TABLE IF NOT EXISTS users (
+    id INTEGER PRIMARY KEY,
+    username VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL
+    )""")
+
+    cur.close()
 
 
 def program_start():
@@ -15,13 +32,13 @@ def program_start():
         user_input = input("Enter your choice: ")
 
         if user_input == '1':
-            print("-" * 20)
+            print()
             print("Welcome back!")
             if login_user():
                 successful_login()
                 running = False
         elif user_input == '2':
-            print("-" * 20)
+            print()
             print("Welcome to registry")
             if user_registration():
                 successful_login()
@@ -33,31 +50,22 @@ def program_start():
 
 def successful_login():
     menu = """\nPassword Manager Menu:
-    [1] - Create a new password file
-    [2] - Load an existing password file
-    [3] - Add a new password
-    [4] - Get a password
+    [1] - Add a new password
+    [2] - Get a password
     [q] - Quit
     \n"""
 
     running = True
 
-    # TODO: Adding error checks
     while running:
         print(menu)
         user_input = input("Enter your choice: ")
 
         if user_input == '1':
-            print("Feature under development")
-            # pm.create_password_file(asking_for_password_path())
-        elif user_input == '2':
-            print("Feature under development")
-            # pm.load_password_file(asking_for_password_path())
-        elif user_input == '3':
             site = input("Enter the site: ")
             password = input("Enter the password: ")
             pm.add_password(site, password)
-        elif user_input == '4':
+        elif user_input == '2':
             password_request = input("Password of what site do you search? ")
             print(f"Password for {password_request} is {pm.get_password(password_request)}")
         elif user_input == 'q' or 'Q':
@@ -67,17 +75,13 @@ def successful_login():
             print("Invalid input!")
 
 
-# def asking_for_key_path():
-#     key_path = './EncKeys/'
-#     return f"{key_path}{input('Enter path: ')}"
-
-
-# def asking_for_password_path():
-#     password_path = "./Users/"
-#     return f"{password_path}{input('Enter path: ')}"
-
-
 def main():
+    # Create users database at the default location
+    create_db()
+
+    # Create Users directory in case if is not created at default location
+    create_directory()
+
     program_start()
 
 
